7fd9f1f7603c7c9d4d1b09e4b129b44023fafbcd
Wiebke <Wiebke@DESKTOP-M110U.fritz.box>
2025-04-24 16:50:20 +0200

    Objektklasse

---
+++ src/idh/java/BenutzerInnen.java
@@ -0,0 +1,11 @@
+package idh.java;
+
+public class BenutzerInnen {
+	int kontonummer;
+	int kontostand;
+	
+	public BenutzerInnen(int kn,int ks) {
+		this.kontonummer = kn;
+		this.kontostand = ks;
+	}
+}
