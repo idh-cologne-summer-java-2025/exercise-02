e9601ea960c07d3b5d217289f4558ede49392dde
Yuima <Yuima@DwieDosenlaptop.local>
2025-04-25 10:01:34 +0200

    Excercises Done. For that I created the new Class Account so the ATM can handle more than one account. yeehaw

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,8 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
-
+	Account[] accArr = {new Account(500),new Account(112), new Account(666)};
+	int cash = 1000;
+	
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
@@ -16,31 +17,72 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print("Enter your Account number: ");
+				int accID = Integer.parseInt(br.readLine());
+				
+				//Check, ob AccID existiert. Wenn nicht, wird die Methode neugestartet
+				//Wenn doch, dann läuft das Programm weiter
+				if (null==getAccountByID(accID)) {
+					run();
+				} 
+				
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount, accID);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
+	public void cashout(int amount, int accID) {
+		
+		Account acc = getAccountByID(accID); 
+		int accountBalance = acc.getAccountBalance();
+		
+		if (amount >= accountBalance) {
+			System.out.println("Sorry, not enough money on your account.");
+		} 
+		else if (amount>=cash) {
+			System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+		}
+		else {
 			accountBalance = accountBalance - amount;
+			cash = cash - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
 		}
 
-	};
+	}
 
+	/**
+	 * Eine Methode, die heraussucht, ob zu der angegebenen Account-ID auch ein Account
+	 * angelegt vorliegt. Wenn nicht, wird eine Fehlermeldung ausgespuckt.
+	 * */
+	public Account getAccountByID(int accID) {
+		for (int i=0;i<accArr.length;i++) {
+			if (accArr[i].getAccountID() == accID) {
+				return accArr[i];
+			}
+		}
+		System.out.println("There's no account with accountID "+accID+". \n "
+				+ "Please type in a valid Account-Number");
+		return null;
+	}
+	
+	public void enoughCash(int amount) {
+		if (amount>= cash) {
+			
+		}
+	}
+	
 	/**
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
 		atm.run();
-	};
+	}
+	
+	
 
 }
---
+++ src/idh/java/Account.java
@@ -0,0 +1,27 @@
+package idh.java;
+
+public class Account {
+	private int accountBalance;
+	private int accountID;
+	
+	public Account (int accountID) {
+		accountBalance = 500;
+		this.accountID = accountID;
+	}
+	
+	
+	
+	
+	
+	protected int getAccountBalance() {
+		return accountBalance;
+	}
+	
+	protected int getAccountID() {
+		return accountID;
+	}
+	
+	protected void setAccountBalance(int accBal) {
+		accountBalance = accBal;
+	}
+}
