816ce691a8c761ecd5bbcebaab33fb9468a067f6
Marc <Marc@LaptopMarc>
2025-04-29 12:30:47 +0200

    Trying to put User Objects into the users-Array

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -5,9 +5,12 @@ import java.io.InputStreamReader;
 //changing sth
 public class ATM {
 	int accountBalance = 1000;
-	User users[] = new User[5]; //Array with UserObjects, needs to be filled though ..., could write a method for creating User Objects and putting them in the Array, not this time though
+	User users[] = new User[2]; //Array with UserObjects, needs to be filled though ... could write a method for creating User Objects and putting them in the Array, not necessary this time.
 	User u1 = new User(1, 50);
 	User u2 = new User(2, 120);
+//	trying to fill users-array with User objects, some kind of syntax error seems to occur 
+//	users[1] = u1;
+//	users[2] = u2;
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
@@ -21,30 +24,31 @@ public class ATM {
 			try {
 				System.out.print("Enter ID: ");
 				int id = Integer.parseInt(br.readLine());
-				for(int i = 0; i < users.length; i++) {
-					if(users[i].getId()==id) {
-						System.out.print("Welcome UserID " + id + "Balance of User " + users[i].getId() + ":"  + users[i].getBalance());
+//				for(int i = 0; i < users.length; i++) {
+//					if(users[i].getId()==id) {
+//						System.out.print("Welcome UserID " + id + "Balance of User " + users[i].getId() + ":"  + users[i].getBalance());
+//		relocate everything above into cashout() 
 						System.out.print("Enter the amount to withdraw: ");
 						int amount = Integer.parseInt(br.readLine());
-						cashout(amount);
-					}else {
-						System.out.println("No such ID.");
+						cashout(id, amount);	
 					}
-					}
-				} catch (Exception e) {
+				 catch (Exception e) {
+				System.err.println("Something went wrong.");
+				e.printStackTrace();
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
+	public void cashout(int id, int amount) {
+		for(int j = 0; j<users.length; j++) {
+		if (id==users[j].getId() && amount < accountBalance) {
 			accountBalance = accountBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
 		}
-
+	}
 	};
 
 	/**
