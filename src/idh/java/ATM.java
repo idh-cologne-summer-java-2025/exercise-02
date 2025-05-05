efe3b365e493f22a26e9e7900d915c16e9305868
ghett <ghett@172.29.181.161>
2025-05-05 17:28:40 +0200

    Aufgabe fertig

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -6,6 +6,14 @@ import java.io.InputStreamReader;
 public class ATM {
 	int accountBalance = 100;
 
+	Account[] accounts = new Account[5];
+	
+	public ATM() {
+		for (int i=0; i < accounts.length; i++) {
+			accounts[i] = new Account(i, 100);
+			
+		}
+	}
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
@@ -16,16 +24,34 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print("Enter the account id to withdraw: ");
+				int id = Integer.parseInt(br.readLine());
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(id, amount);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
+	protected Account getAccount(int id) {
+		for (int i=0; i < accounts.length; i++) {
+			if (accounts[i].getId() == id) {
+				return accounts[i];
+			}
+		}
+		 return null;
+	}
+	public void cashout(int id, int amount) {
+		
+		Account account = getAccount(id);
+		
+		if(account == null) {
+			System.out.println("dieses Konto existiert nicht");
+			return;
+		}
+		
 		if (amount < accountBalance) {
 			accountBalance = accountBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
